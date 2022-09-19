a7571bc04929592262b81284dead7cd02f5d2b3d
이건후 <gh@igeonhuui-MacBookAir.local>
2022-09-19 22:17:45 +0900

    feat: 백그라운드 이미지, 로그인 퍼블리싱

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from 'pages/home/Home';
 import Leagues from 'pages/leagues/Leagues';
@@ -15,18 +14,19 @@ function App() {
   return (
       <BrowserRouter>
       <NavBar />
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/leagues' element={<Leagues/>}></Route>
-          <Route path='/rankings' element={<Rankings/>}></Route>
-          <Route path='/university' element={<University/>}></Route>
-          
-          <Route path='/login' element={<Login/>}></Route>
-          <Route path='/mypage' element={<MyPage/>}></Route>
-          <Route path='/singup' element={<SingUp/>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <div className='background'>
 
+        <Routes>
+        <Route path='/' element={<Home />}></Route>
+          <Route path='/leagues' element={<Leagues />}></Route>
+          <Route path='/rankings' element={<Rankings />}></Route>
+          <Route path='/university' element={<University />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/mypage' element={<MyPage />}></Route>
+          <Route path='/singup' element={<SingUp />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
