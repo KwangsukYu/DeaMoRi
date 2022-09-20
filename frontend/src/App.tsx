a8f64ebe085291a5472b85605f1f0a867f55fb63
이건후 <lgh3806@naver.com>
2022-09-20 15:02:06 +0900

    feat:로그인, 회원가입, 회원정보 수정 퍼블리싱

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from 'pages/home/Home';
-import Leagues from 'pages/leagues/Leagues';
-import Rankings from 'pages/rankings/Rankings';
-import University from 'pages/university/University';
-import NavBar from 'components/navBar/NavBar';
-  
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "pages/home/Home";
+import Leagues from "pages/leagues/Leagues";
+import Rankings from "pages/rankings/Rankings";
+import University from "pages/university/University";
+import NavBar from "components/navBar/Navbar";
+
 // user
-import Login from 'pages/user/Login';
-import MyPage from 'pages/user/MyPage';
-import SingUp from 'pages/user/SignUp';
+import Login from "pages/user/Login";
+import MyPage from "pages/user/MyPage";
+import SingUp from "pages/user/SignUp";
+import Edit from "pages/user/Edit";
 
 function App() {
   return (
-      <BrowserRouter>
+    <BrowserRouter>
       <NavBar />
-      <div className='background'>
-
+      <div className="background">
         <Routes>
-        <Route path='/' element={<Home />}></Route>
-          <Route path='/leagues' element={<Leagues />}></Route>
-          <Route path='/rankings' element={<Rankings />}></Route>
-          <Route path='/university' element={<University />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/mypage' element={<MyPage />}></Route>
-          <Route path='/singup' element={<SingUp />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/leagues" element={<Leagues />}></Route>
+          <Route path="/rankings" element={<Rankings />}></Route>
+          <Route path="/university" element={<University />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/mypage" element={<MyPage />}></Route>
+          <Route path="/signup" element={<SingUp />}></Route>
+          <Route path="/edit" element={<Edit />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
