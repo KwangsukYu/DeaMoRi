ef7ed40e19950c8e79fd8a71854828a2fc243529
KwangsukYu <soye0822@gmail.com>
2022-09-20 16:41:16 +0900

    feat : navbar 퍼블리싱

--- frontend/src/pages/user/MyPage.tsx
+++ frontend/src/pages/user/MyPage.tsx
@@ -1,8 +1,4 @@
 function MyPage() {
-    return(
-        <div>
-            여긴 MyPage 페이지
-        </div>
-    )
+  return <div>여긴 MyPage 페이지</div>;
 }
-export default MyPage
+export default MyPage;
