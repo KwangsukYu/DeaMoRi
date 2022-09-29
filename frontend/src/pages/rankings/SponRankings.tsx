90afef9c80387dc115ff8a7f6879e5c63847564f
이건후 <lgh3806@naver.com>
2022-09-29 17:07:10 +0900

    feat:후원 랭킹 구현

--- frontend/src/pages/rankings/SponRankings.tsx
+++ frontend/src/pages/rankings/SponRankings.tsx
@@ -1,13 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Rankings.scss";
 import rankLogo1 from "assets/images/rank1.png";
 import rankLogo2 from "assets/images/rank2.png";
 import rankLogo3 from "assets/images/rank3.png";
 import rankLogo4 from "assets/images/rank4.png";
 import axios from "axios";
+import { v4 } from "uuid";
 import MySponRank from "./MySponRank";
 
+interface userRankType {
+  userPk: number;
+  ranking: number;
+  donation: number;
+  nickName: string;
+  userName: string;
+  badge: string;
+  universityPk: number;
+  universityName: null;
+  universityLogo: null;
+}
+
 function SponRankings() {
+  const [userRank, setUserRank] = useState<userRankType[]>([]);
   useEffect(() => {
     axios({
       url: "http://j7c208.p.ssafy.io:8080/api/ranking/user",
@@ -18,46 +32,53 @@ function SponRankings() {
     })
       .then(res => {
         console.log(res);
+        setUserRank(res.data.userRankings);
       })
       .catch(err => {
         console.error(err);
       });
-  });
+  }, []);
 
-  const universities = [
-    { rank: 1, name: "홍석호홍석호홍석호", price: "5000000" },
-    { rank: 2, name: "유광석", price: "4000000" },
-    { rank: 3, name: "이민재", price: "3000000" },
-    { rank: 4, name: "김성민", price: "2000000" },
-    { rank: 5, name: "이성조", price: "1000000" }
-  ];
-  const RankMain = universities.map(uni => {
+  // const universities = [
+  //   { rank: 1, name: "홍석호홍석호홍석호", price: "5000000" },
+  //   { rank: 2, name: "유광석", price: "4000000" },
+  //   { rank: 3, name: "이민재", price: "3000000" },
+  //   { rank: 4, name: "김성민", price: "2000000" },
+  //   { rank: 5, name: "이성조", price: "1000000" }
+  // ];
+
+  const RankMain = userRank.map(user => {
     let rankImg = "";
     let rankClass = "";
-    if (uni.rank === 1) {
+    console.log(user);
+    if (user.ranking === 1) {
       rankImg = rankLogo1;
       rankClass = "rankLogo1";
-    } else if (uni.rank === 2) {
+    } else if (user.ranking === 2) {
       rankImg = rankLogo2;
       rankClass = "rankLogo2";
-    } else if (uni.rank === 3) {
+    } else if (user.ranking === 3) {
       rankImg = rankLogo3;
       rankClass = "rankLogo3";
     } else {
       rankImg = rankLogo4;
       rankClass = "rankLogo4";
     }
+
     return (
-      <div key={uni.rank} className="rank-main">
+      <div key={v4()} className="rank-main">
         <div className="logo-box">
           <img className={rankClass} src={rankImg} alt="" />
-          <p className="rank-main-rank">{uni.rank}</p>
+          <p className="rank-main-rank">{user.ranking}</p>
         </div>
-        <p className="rank-main-name">{uni.name}</p>
-        <p className="rank-main-price">{uni.price} MOKO</p>
+        <p className="rank-main-name">{user.nickName}</p>
+        <p className="rank-main-price">{user.donation} MOKO</p>
       </div>
     );
   });
+
+  console.log(userRank);
+
   return (
     <div id="rank-box">
       <div className="rank-menu">
@@ -66,11 +87,11 @@ function SponRankings() {
         <p className="rank-menu-price">후원금</p>
       </div>
       <div>
-        <p className="my-uni-title">마이 대학 순위</p>
+        <p className="my-uni-title">내 후원 순위</p>
         <MySponRank />
         <div className="line" />
       </div>
-      <p className="total-uni-title">전체 대학 순위</p>
+      <p className="total-uni-title">전체 후원 순위</p>
       {RankMain}
     </div>
   );
