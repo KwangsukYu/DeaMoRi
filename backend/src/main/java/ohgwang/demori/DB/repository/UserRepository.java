9c062ab4aeafdee6a27f5b0577d0a578446e3cc9
이민재 <uiui96@naver.com>
2022-09-29 15:36:10 +0900

    fix : UserRankingRes 수정

--- backend/src/main/java/ohgwang/demori/DB/repository/UserRepository.java
+++ backend/src/main/java/ohgwang/demori/DB/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package ohgwang.demori.DB.repository;
 
 import ohgwang.demori.DB.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -22,5 +24,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     List<Integer> getRank();
 
     List<User> findAllByOrderByDonationDesc();
+
+    Page<User> findAll(Pageable pageable);
 }
 
