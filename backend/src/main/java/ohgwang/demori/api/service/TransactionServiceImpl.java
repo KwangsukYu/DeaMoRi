0ac1fbf28fa87e09b428094febcfb428a136bb90
Seokho Hong <seokho.hong.dev@gmail.com>
2022-10-05 14:16:22 +0900

    fix : web3j url 변경

--- backend/src/main/java/ohgwang/demori/api/service/TransactionServiceImpl.java
+++ backend/src/main/java/ohgwang/demori/api/service/TransactionServiceImpl.java
@@ -20,7 +20,7 @@ import java.util.Map;
 @Service
 public class TransactionServiceImpl implements TransactionService {
 
-    private final Web3j web3j = Web3j.build(new HttpService("http://j7c2081.p.ssafy.io:8545"));
+    private final Web3j web3j = Web3j.build(new HttpService("https://j7c2081.p.ssafy.io/blockchain"));
 
     @Autowired
     LeagueRepository leagueRepository;
