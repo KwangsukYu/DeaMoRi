f81c8b1106280436e6d4c88f10799ff5b24b2384
이건후 <lgh3806@naver.com>
2022-10-05 13:55:31 +0900

    feat:비로그인 시 랭킹 페이지 구현

--- frontend/src/pages/rankings/MySponRank.tsx
+++ frontend/src/pages/rankings/MySponRank.tsx
@@ -7,64 +7,103 @@ import rankLogo4 from "assets/images/rank4.png";
 import { infoType } from "Slices/userInfo";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { getInfo } from "../../Slices/userInfo";
 
 function MySponRank() {
   const storeUser = useSelector((state: infoType) => state.userInfo.userInfo);
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
   useEffect(() => {
-    axios({
-      url: "https://j7c208.p.ssafy.io:8080/api/users/me",
-      method: "get",
-      headers: { Authorization: `Bearer ${localStorage.token}` }
-    })
-      .then(res => {
-        console.log(res.data, "로그인 시 스토어 저장 데이터");
-        dispatch(getInfo(res.data));
+    if (localStorage.token) {
+      axios({
+        url: "https://j7c208.p.ssafy.io:8080/api/users/me",
+        method: "get",
+        headers: { Authorization: `Bearer ${localStorage.token}` }
       })
-      .catch(err => {
-        console.error(err);
-      });
+        .then(res => {
+          console.log(res.data, "로그인 시 스토어 저장 데이터");
+          dispatch(getInfo(res.data));
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
   }, []);
 
   console.log(storeUser);
-  const myUni = { rank: 123, name: "김싸피", price: "20000" };
   let rankImg = "";
   let rankClass = "";
-  if (storeUser.ranking === 1) {
-    rankImg = rankLogo1;
-    rankClass = "rankLogo1";
-  } else if (storeUser.ranking === 2) {
-    rankImg = rankLogo2;
-    rankClass = "rankLogo2";
-  } else if (storeUser.ranking === 3) {
-    rankImg = rankLogo3;
-    rankClass = "rankLogo3";
-  } else {
-    rankImg = rankLogo4;
-    rankClass = "rankLogo4";
+  if (localStorage.token) {
+    if (storeUser.ranking === 1) {
+      rankImg = rankLogo1;
+      rankClass = "rankLogo1";
+    } else if (storeUser.ranking === 2) {
+      rankImg = rankLogo2;
+      rankClass = "rankLogo2";
+    } else if (storeUser.ranking === 3) {
+      rankImg = rankLogo3;
+      rankClass = "rankLogo3";
+    } else {
+      rankImg = rankLogo4;
+      rankClass = "rankLogo4";
+    }
   }
+
   return (
-    <button type="button" key={storeUser.ranking} className="rank-main">
-      {storeUser.donation === 0 ? (
-        <div className="logo-box-my">
-          <img className={rankClass} src={rankLogo4} alt="" />
-          <p className="rank-main-unrank">-</p>
-        </div>
+    <div>
+      {localStorage.token ? (
+        <button type="button" key={storeUser.ranking} className="rank-main">
+          {storeUser.donation === 0 ? (
+            <div className="logo-box-my">
+              <img className={rankClass} src={rankLogo4} alt="" />
+              <p className="rank-main-unrank">-</p>
+            </div>
+          ) : (
+            <div className="logo-box-my">
+              <img className={rankClass} src={rankImg} alt="" />
+              <p className="rank-main-rank">{storeUser.ranking}</p>
+            </div>
+          )}
+
+          <p className="rank-main-name">{storeUser.nickName}</p>
+          <p className="rank-main-price">
+            {storeUser.donation
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+            WON
+          </p>
+        </button>
       ) : (
-        <div className="logo-box-my">
-          <img className={rankClass} src={rankImg} alt="" />
-          <p className="rank-main-rank">{storeUser.ranking}</p>
+        <div>
+          <div className="rank-main">
+            <div className="no-uni-box">
+              <p>로그인이 필요합니다.</p>
+              <div className="login-signup-box">
+                <button
+                  type="button"
+                  className="go-button"
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                >
+                  로그인
+                </button>
+                <button
+                  type="button"
+                  className="go-button"
+                  onClick={() => {
+                    navigate("/signup");
+                  }}
+                >
+                  회원가입
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       )}
-
-      <p className="rank-main-name">{storeUser.nickName}</p>
-      <p className="rank-main-price">
-        {storeUser.donation.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
-        WON
-      </p>
-    </button>
+    </div>
   );
 }
 
